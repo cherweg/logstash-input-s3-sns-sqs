5cfc49b3ba46443d773c1647263b174c4861c606
Christian Herweg <christian.herweg@otto.de>
2019-06-14 11:54:02 +0200

    [WIP] Fixed codec by folder, assume_role, pa_config

--- lib/logstash/inputs/s3/client_factory.rb
+++ lib/logstash/inputs/s3/client_factory.rb
@@ -13,6 +13,7 @@ module LogStash module Inputs class S3SNSSQS < LogStash::Inputs::Threadable
       @sts_client = Aws::STS::Client.new(region: options[:aws_region])
       # FIXME: options are non-generic (...by_bucket mixes credentials with folder stuff)
       @credentials_by_bucket = options[:s3_credentials_by_bucket]
+      @logger.info("Credetials by Bucket", :credentials => @credentials_by_bucket)
       # lazy-init this as well:
       # @credentials_by_bucket = @options_by_bucket.map { |bucket, options|
       #   [bucket.to_sym, assume_s3_role(options['credentials'])]
@@ -26,11 +27,13 @@ module LogStash module Inputs class S3SNSSQS < LogStash::Inputs::Threadable
     def get_s3_client(bucket_name)
       bucket_symbol = bucket_name.to_sym
       @creation_mutex.synchronize do
-        @logger.info("Inside factory mutex", :client => @clients_by_bucket[bucket_symbol].nil?)
+
         if @clients_by_bucket[bucket_symbol].nil?
           options = @aws_options_hash
-          if @credentials_by_bucket[bucket_name]
+          @logger.info("Trying to merge aws options", :before_options => options, :bucket_options => @credentials_by_bucket[bucket_name])
+          if @credentials_by_bucket.key?(bucket_name)
             options.merge!(credentials: assume_s3_role(@credentials_by_bucket[bucket_name]))
+            @logger.info("Merged aws options", :used_options => options)
           end
           @clients_by_bucket[bucket_symbol] = Aws::S3::Client.new(options)
           @logger.info("Created a new S3 Client", :bucket_name => bucket_name, :client => @clients_by_bucket[bucket_symbol])
@@ -52,6 +55,7 @@ module LogStash module Inputs class S3SNSSQS < LogStash::Inputs::Threadable
 
     def assume_s3_role(credentials)
       # reminder: these are auto-refreshing!
+      @logger.info("Assume Role", :credentials => credentials["role"])
       return Aws::AssumeRoleCredentials.new(
           client: @sts_client,
           role_arn: credentials['role'],
