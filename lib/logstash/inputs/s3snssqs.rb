89db6ed92ebee62e53455a3741f0ebac8008028e
Christian Herweg <christian.herweg@otto.de>
2019-06-25 17:28:39 +0200

    [WiP] Debug threadding problem in log_processor

--- lib/logstash/inputs/s3snssqs.rb
+++ lib/logstash/inputs/s3snssqs.rb
@@ -265,8 +265,10 @@ class LogStash::Inputs::S3SNSSQS < LogStash::Inputs::Threadable
   def run(logstash_event_queue)
     #LogStash::ShutdownWatcher.abort_threshold(30)
     # start them
-    @worker_threads = @consumer_threads.times.map do |_|
-      run_worker_thread(logstash_event_queue)
+    @queue_mutex = Mutex.new
+    #@consumer_threads= 1
+    @worker_threads = @consumer_threads.times.map do |thread_id|
+      run_worker_thread(logstash_event_queue, thread_id)
     end
     # and wait (possibly infinitely) for them to shut down
     @worker_threads.each { |t| t.join }
@@ -290,7 +292,7 @@ class LogStash::Inputs::S3SNSSQS < LogStash::Inputs::Threadable
 
   private
 
-  def run_worker_thread(queue)
+  def run_worker_thread(queue, thread_id)
     Thread.new do
       @logger.info("Starting new worker thread")
       @sqs_poller.run do |record|
@@ -298,6 +300,7 @@ class LogStash::Inputs::S3SNSSQS < LogStash::Inputs::Threadable
         @logger.debug("Outside Poller: got a record", :record => record)
         # record is a valid object with the keys ":bucket", ":key", ":size"
         record[:local_file] = File.join(@temporary_directory, File.basename(record[:key]))
+        LogStash::Util.set_thread_name("[Processor #{thread_id} -  Working on: #{record[:key]}")
         if @s3_downloader.copy_s3object_to_disk(record)
           completed = catch(:skip_delete) do
             process(record, queue)
